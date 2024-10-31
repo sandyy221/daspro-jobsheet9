e7be5e6b2beecc4166d91e97393eae9aaaee339f
sandyy221 <kurniawansandy808.com>
2024-10-31 13:49:24 +0700

    modifikasi kode pertanyaan

--- ArrayNilai27.java
+++ ArrayNilai27.java
@@ -8,9 +8,13 @@ public class ArrayNilai27 {
             System.out.print("Masukan nilai akhir ke-" +i+ " :");
             nilaiAkhir [i] = sc.nextInt();
         }
-        for (int i = 0; i<10; i++) {
-            System.out.println("Nilai akhir ke-" +i+ " adalah " + nilaiAkhir[i]);
-
+        for (int i = 0; i<nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" +i+ " lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-" +i+" tidak lulus!");
+            }
+         
         }
     }
     
