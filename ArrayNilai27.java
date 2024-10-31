539c2cb67e40ffa1123a9be35babaca398b67e8d
sandyy221 <kurniawansandy808.com>
2024-10-31 12:02:46 +0700

    percobaan2

--- ArrayNilai27.java
+++ ArrayNilai27.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ArrayNilai {
+public class ArrayNilai27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int [] nilaiAkhir = new int[10];
